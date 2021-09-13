f886aeb49fbcbb521a79cc0656b064780315660f
ayousry <ayousry@sumerge.com>
2021-09-14 01:42:22 +0200

    [master]: adding gatewaylist component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {GatewayListComponent} from './gateway-list/gateway-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: GatewayListComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ src/app/gateway-list/gateway-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gateway-list',
+  templateUrl: './gateway-list.component.html',
+  styleUrls: ['./gateway-list.component.css']
+})
+export class GatewayListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
