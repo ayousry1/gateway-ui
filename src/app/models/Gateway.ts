ae3ab6ca4125e9eebd6d73c7305a87307ec03eaa
ayousry <ayousry@sumerge.com>
2021-09-14 03:13:49 +0200

    [master]: adding models

---
+++ src/app/models/Gateway.ts
@@ -0,0 +1,16 @@
+import {Device} from './Device';
+
+export class Gateway {
+  serialNumber: string;
+  name: string;
+  ip4Address: string;
+  peripheralDevices: Device[];
+
+
+  constructor(serialNumber: string, name: string, ip4Address: string, peripheralDevices: Device[]) {
+    this.serialNumber = serialNumber;
+    this.name = name;
+    this.ip4Address = ip4Address;
+    this.peripheralDevices = peripheralDevices;
+  }
+}
